81886e6010eb842cffe8b9d098269170d3de0b6a
Jiri Popelka <jpopelka@redhat.com>
2020-06-22 20:39:48 +0200

    New delete-pvcs CronJob to delete old PVCs in sandbox
    Work-around for
    https://github.com/packit-service/packit-service/issues/409

---
+++ cron-jobs/delete-pvcs/delete-pvcs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+oc login "${HOST}" --token="${TOKEN}"
+
+set -x
+
+NAMESPACE="packit-${DEPLOYMENT}-sandbox"
+
+oc get pvc -n "${NAMESPACE}" | grep pvc |
+while IFS= read -r line; do
+  age=$(echo $line | awk '{print $7}')
+  if [ "${age: -1}" == "d" ]; # days
+  then
+    name=$(echo $line | awk '{print $1}')
+    echo "deleting ${name}"
+    oc delete pvc "${name}" -n "${NAMESPACE}"
+  fi
+done
