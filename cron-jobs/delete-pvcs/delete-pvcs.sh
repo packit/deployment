8a594067c1f4d19c6c5e273e6a4b623491bb2691
Jiri Popelka <jpopelka@redhat.com>
2021-04-07 14:29:39 +0200

    Also delete pods older than a day

--- cron-jobs/delete-pvcs/delete-pvcs.sh
+++ cron-jobs/delete-pvcs/delete-pvcs.sh
@@ -12,13 +12,21 @@ POD_NAME_STARTSWITH="quay-io-packit-sandcastle-${DEPLOYMENT}-"
 oc get pod -n "${NAMESPACE}" | grep "^${POD_NAME_STARTSWITH}" |
 # for each pod
 while IFS= read -r line; do
-  pod_name=$(echo "${line}" | awk '{print $1}')
   pod_status=$(echo "${line}" | awk '{print $3}')
   if [ "${pod_status}" == "Completed" ];
   then
+    pod_name=$(echo "${line}" | awk '{print $1}')
+    pod_age=$(echo "${line}" | awk '{print $5}')
+
     # get associated PVC name
     pvc_name=$(oc get pod "${pod_name}" -n "${NAMESPACE}" -o=jsonpath="{.spec.volumes[0].persistentVolumeClaim.claimName}")
-    echo "Deleting ${pvc_name}"
+    echo "Deleting pvc ${pvc_name}"
     oc delete pvc "${pvc_name}" -n "${NAMESPACE}"
+
+    if [ "${pod_age: -1}" == "d" ]; # days
+    then
+      echo "Deleting pod ${pod_name}"
+      oc delete pod "${pod_name}" -n "${NAMESPACE}"
+    fi
   fi
 done
