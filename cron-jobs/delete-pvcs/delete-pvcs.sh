6fec9ba2ea02df3c3ddbd9e4250692689f8b9012
Hunor Csomortáni <csomh@redhat.com>
2021-08-04 16:46:21 +0200

    Improve deleting stale sandbox PVCs
    The previous approach checked for stale pods first and deleted the
    corresponding PVCs. But as we found out in the meantime, sometimes pods
    are successfully removed, but the PVCs requested by them are not. These
    PVCs would be never considered for deletion.
    
    Change the approach to first delete stale pods, then identify all the
    sandbox PVCs that are mounted in non-stale pods and then delete any
    sandbox PVC that is not mounted.
    
    Set up some basic testing.
    
    Signed-off-by: Hunor Csomortáni <csomh@redhat.com>

--- cron-jobs/delete-pvcs/delete-pvcs.sh
+++ cron-jobs/delete-pvcs/delete-pvcs.sh
@@ -1,32 +1,62 @@
 #!/usr/bin/bash
 
-oc login "${HOST}" --token="${TOKEN}"
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
 
-set -x
+set -eu
+
+# Set TEST=yes to run the script with a mocked oc.
+# See test.sh for test outputs.
+TEST="${TEST:-}"
+
+myoc() {
+    oc -n "${NAMESPACE}" "$@"
+}
+
+if [[ -n "${TEST}" ]]; then
+    source "$(dirname $0)/test.sh"
+fi
 
 DEFAULT_NAMESPACE="packit-${DEPLOYMENT}-sandbox"
 NAMESPACE="${SANDBOX_NAMESPACE:-$DEFAULT_NAMESPACE}"
-POD_NAME_STARTSWITH="quay-io-packit-sandcastle-${DEPLOYMENT}-"
+POD_NAME_PREFIX="quay-io-packit-sandcastle-${DEPLOYMENT}-"
+PVC_NAME_PREFIX="sandcastle--sandcastle"
+JSONPATH="{.spec.volumes[:].persistentVolumeClaim.claimName}"
 
-# get all pods with name starting with POD_NAME_STARTSWITH
-oc get pod -n "${NAMESPACE}" | grep "^${POD_NAME_STARTSWITH}" |
-# for each pod
+myoc login "${HOST}" --token="${TOKEN}"
+
+# ============================================================================
+# Delete Completed and OOMKilled sandbox pods older than a day.
+# ============================================================================
+echo "Checking pods..."
+mounted_pvcs=""
 while IFS= read -r line; do
-  pod_status=$(echo "${line}" | awk '{print $3}')
-  if [[ "${pod_status}" == "Completed" || "${pod_status}" == "OOMKilled" ]];
-  then
+    [[ -z "${line}" ]] && continue
     pod_name=$(echo "${line}" | awk '{print $1}')
+    pod_status=$(echo "${line}" | awk '{print $3}')
     pod_age=$(echo "${line}" | awk '{print $5}')
 
-    # get associated PVC name
-    pvc_name=$(oc get pod "${pod_name}" -n "${NAMESPACE}" -o=jsonpath="{.spec.volumes[0].persistentVolumeClaim.claimName}")
-    echo "Deleting pvc ${pvc_name}"
-    oc delete pvc "${pvc_name}" -n "${NAMESPACE}"
-
-    if [[ "${pod_age: -1}" == "d" ]]; # days
+    if [[ "${pod_status}" == "Completed" || "${pod_status}" == "OOMKilled" ]] \
+        && [[ "${pod_age: -1}" == "d" ]]; # days
     then
-      echo "Deleting pod ${pod_name}"
-      oc delete pod "${pod_name}" -n "${NAMESPACE}"
+        echo "Deleting pod ${pod_name}"
+        myoc delete --ignore-not-found pod "${pod_name}"
+    else
+        # Get associated PVC name.
+        # Multiple values are separated by space.
+        mounted_pvcs="${mounted_pvcs} $(myoc get pod "${pod_name}" -o=jsonpath=${JSONPATH})"
     fi
-  fi
-done
+done <<<$(myoc get pod | grep "^${POD_NAME_PREFIX}")
+# Use awk to filter out empty lines. Grep would exit non-zero if mounted_pvcs is empty.
+mounted_pvcs=$(echo "${mounted_pvcs}" | tr ' ' '\n' | awk '/./ {print $0}')
+
+# ============================================================================
+# Delete the sandbox PVCs which are not mounted in any pod.
+# ============================================================================
+echo "Checking PVCs..."
+all_pvcs=$(myoc get pvc | awk '$0 !~ "NAME" {print $1}')
+while IFS= read -r pvc; do
+    [[ -z "${pvc}" ]] && continue
+    echo "Deleting pvc ${pvc}"
+    myoc delete --ignore-not-found pvc "${pvc}"
+done <<<$(echo -e "${mounted_pvcs}\n${all_pvcs}" | grep "^${PVC_NAME_PREFIX}" | sort | uniq -u)
