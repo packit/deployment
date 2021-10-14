48ef0547c38300dbd7ad125385c648c5c2f25b66
Hunor Csomortáni <csomh@redhat.com>
2021-10-14 10:18:29 +0200

    Write a script to generate secret tokens for GitLab webhooks
    Signed-off-by: Hunor Csomortáni <csomh@redhat.com>

---
+++ scripts/gitlab_webhook.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
+
+import click
+import jwt
+import yaml
+
+
+@click.command()
+@click.argument("service_config")
+@click.argument("namespace")
+@click.argument("repo_name", required=False)
+def generate_webhook(service_config, namespace, repo_name):
+    """Generate a secret token to be used to set up webhooks in GitLab.
+
+    SEVICE_CONFIG is a Packit-as-a-Service configuration file, having a
+    'gitlab_token_secret key', holding the secret to generate webhook
+    tokens.
+
+    NAMESPACE is all the groups and subgroups (if any), which are the
+    parents of the repository, separated by '/'.
+
+    The optional REPO_NAME is the name of the repo for which the token
+    is generated. Leave it out if you generate a token for the namespace.
+    """
+    with open(service_config, "r") as fp:
+        data = yaml.safe_load(fp)
+        gitlab_token_secret = data["gitlab_token_secret"]
+    payload = {"namespace": namespace}
+    if repo_name:
+        payload["repo_name"] = repo_name
+    print(jwt.encode(payload, gitlab_token_secret, algorithm="HS256").decode("utf-8"))
+
+
+if __name__ == "__main__":
+    generate_webhook()
