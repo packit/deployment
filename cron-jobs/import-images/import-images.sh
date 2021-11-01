4ae2bab2d4c450e44dc0255bd67cb9976205bdb9
Jiri Popelka <jpopelka@redhat.com>
2021-11-01 11:34:53 +0100

    Allow to specify service
    Some image streams are common to both services,
    some are packit service exclusive.

--- cron-jobs/import-images/import-images.sh
+++ cron-jobs/import-images/import-images.sh
@@ -4,11 +4,16 @@ oc login "${HOST}" --token="${TOKEN}" --insecure-skip-tls-verify
 
 set -x
 
-DEFAULT_NAMESPACE="packit-${DEPLOYMENT}"
+DEFAULT_SERVICE="packit"
+SERVICE="${SERVICE:-$DEFAULT_SERVICE}"
+DEFAULT_NAMESPACE="${SERVICE}-${DEPLOYMENT}"
 NAMESPACE="${NAMESPACE:-$DEFAULT_NAMESPACE}"
 
 oc import-image is/packit-service:"${DEPLOYMENT}" -n "${NAMESPACE}"
 oc import-image is/packit-worker:"${DEPLOYMENT}" -n "${NAMESPACE}"
-oc import-image is/packit-dashboard:"${DEPLOYMENT}" -n "${NAMESPACE}"
-oc import-image is/packit-service-fedmsg:"${DEPLOYMENT}" -n "${NAMESPACE}"
-oc import-image is/tokman:"${DEPLOYMENT}" -n "${NAMESPACE}"
+
+if [[ "${SERVICE}" == "packit" ]]; then
+  oc import-image is/packit-dashboard:"${DEPLOYMENT}" -n "${NAMESPACE}"
+  oc import-image is/packit-service-fedmsg:"${DEPLOYMENT}" -n "${NAMESPACE}"
+  oc import-image is/tokman:"${DEPLOYMENT}" -n "${NAMESPACE}"
+fi
