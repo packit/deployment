214864198b111a99454ba63011fd8b51b9f576ab
Frantisek Lachman <flachman@redhat.com>
2022-02-14 10:40:59 +0100

    Add script for getting changelog entries from the merge commits
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

---
+++ scripts/changelog.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
+
+from typing import List
+
+import click
+from git import Commit, Repo
+
+NOT_IMPORTANT_VALUES = ["n/a", "none", "none."]
+
+
+def get_relevant_commits(repository: Repo, tag: str) -> List[Commit]:
+    range = f"{tag}..HEAD"
+    return list(repository.iter_commits(rev=range, merges=True))
+
+
+def convert_message(message: str) -> str:
+    cleared_message = message.split("Reviewed-by")[0].strip()
+    return cleared_message.split("\n\n")[-1].strip()
+
+
+def get_changelog(commits: List[Commit]) -> str:
+    changelog = ""
+    for commit in commits:
+        message = convert_message(commit.message)
+        if message.lower() not in NOT_IMPORTANT_VALUES:
+            changelog += f"- {message}\n"
+    return changelog
+
+
+@click.command(
+    short_help="Get the changelog from the merge commits",
+    help="""Get the changelog from the merge commits
+
+    The script goes through the merge commits since the specified TAG
+    and get the changelog entry from the commit message.
+    By now, we parse a last paragraph of the pull-request description
+    (that is contained in the commit message).
+    In the future, we will have an explicit divider.
+    """,
+)
+@click.argument("tag", type=click.STRING)
+def changelog(tag):
+    print(get_changelog(get_relevant_commits(Repo("."), tag)))
+
+
+if __name__ == "__main__":
+    changelog()
